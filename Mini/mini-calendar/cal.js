01a6417cf09acfba67380e3451f057c0f55a936c
RenukaCode <renukadevibonam3377@gmail.com>
2025-06-09 03:05:21 +0530

    on process

--- Mini/mini-calendar/cal.js
+++ Mini/mini-calendar/cal.js
@@ -119,3 +119,38 @@ function gotoDate(){
     }
     alert("invalid date");
 }
+
+const addEventBtn = document.querySelector(".add-event"),
+      addEventContainer = document.querySelector(".add-event-wrapper"),
+      addEventCloseBtn = document.querySelector(".close");
+      addEventTitle = document.querySelector("event-name"),
+      addEventFrom = document.querySelector("event-time-from"),
+      addEventTo = document.querySelector("event-time-to");
+
+addEventBtn.addEventListener("click", () => {
+    addEventContainer.classList.toggle("active");
+});
+
+addEventCloseBtn.addEventListener("click", () => {
+    addEventContainer.classList.remove("active");
+});
+
+document.addEventListener("click",(e) =>{
+    if(e.target !== addEventBtn && !addEventContainer.contains(e.target) ){
+        addEventContainer.classList.remove("active");
+    }
+})
+
+// addEventTitle.addEventListener("input", (e) => {
+//     addEventTitle.value = addEventTitle.value.slice(0, 50);
+// });
+
+// addEventFrom.addEventListener("input", (e) => {
+//     addEventFrom.value = addEventFrom.value.replace(/[^0-9:]/g, "");
+//     if(addEventFrom.value.length === 2){
+//         addEventFrom.value += ":";
+//     }
+//     if(addEventFrom.value.length > 5){
+//         addEventFrom.value = addEventFrom.value.slice(0, 5);
+//     }
+// })
